c9ea1ae72a9bf7935c5ac348c98161cd3a13e149
Oscar Wurdinger <oscar.wurdinger@garmin.com>
2023-07-26 15:47:26 -0700

    Attempting to get lsp to run
    Doing things to get the clangd lsp running correctly so that I can
    use that feature set!

--- lua/deps/lsp.lua
+++ lua/deps/lsp.lua
@@ -9,19 +9,61 @@ return {
             },
             function()
                 require("mason").setup()
-            end,
+                require("mason-lspconfig").setup()
+                -- require("nvim-lspconfig").setup()
 
-            function()
-                require("mason-lspconfig").setup {
-                ensure_installed = { "sumneko_lua", "clangd" },
+                require("mason-lspconfig").setup_handlers {
+                    function(clangd)
+                        -- require("lspconfig").clangd.setup {}
+                    end,
+
+                    -- ["clangd"] = function()
+                    --     require("").setup {}
+                    -- end
                 }
             end,
 
-            function()
-                require("lspconfig").sumneko_lua.setup()
-            end,
+            -- function()
+            --     require("mason-lspconfig").setup {
+            --     ensure_installed = { "lua_ls", "clangd" },
+            --     automatic_installation = true,
+            --     }
+            --
+            --     require("mason-lspconfig").setup_handlers {
+            --         function()
+            --             require("lspconfig").clangd.setup {}
+            --         end,
+            --     }
+            -- end,
+            --
+            -- function()
+            --     require("lspconfig").clangd.setup()
+            --     require("lspconfig").sumneko_lua.setup()
+            -- end,
 
     },
+    {
+        "neovim/nvim-lspconfig"
+    },
+    {
+    "williamboman/mason-lspconfig.nvim",
+    deps = {
+        requires = "williamboman/mason.nvim",
+                   "neovim/nvim-lspconfig",
+    },
+
+    function()
+        -- require("nvim-lspconfig").cland.setup {}
+
+
+        -- require("mason-lspconfig").setup()
+        -- require("mason-lspconfig").setup {
+        -- ensure_installed = { "lua_ls", "clangd" },
+        -- }
+    end,
+
+    },
+
 }
 
 
